1ac37d657c86cedd606bf5a3d3ced12462ebc4c3
arnaud <arnaud@idelub.com>
2021-05-17 23:46:25 +0200

    adding env file to workflow

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,11 +1,18 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutterissuesapp/app/app.dart';
+import 'package:flutterissuesapp/issues/bloc/bloc.dart';
 import 'package:flutterissuesapp/issues/issues.dart';
 import 'package:flutterissuesapp/issues/views/details/details_page.dart';
+import 'package:github_api_repository/github_api_repository.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:mocktail/mocktail.dart';
 
 import 'helpers/initiate_app.dart';
+
+class MockDetailsBloc extends Mock implements DetailsBloc {}
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -28,11 +35,50 @@ void main() {
 
   testWidgets('Should navigate to details page when an issue card is tapped',
       (WidgetTester tester) async {
+    final mockDetailsBloc = MockDetailsBloc();
     const issueCardFinderTest = Key('issue card 1');
-    await tester.pumpWidget(const App());
+    const testIssue = Issue(
+        id: 'foo',
+        author:
+            Author(login: 'Arnaud', avatarUrl: 'https://placholder.com/250'),
+        title: 'title',
+        state: 'open',
+        bodyText: 'hello world',
+        createdAt: 'created date',
+        updatedAt: 'update date',
+        comments: EdgeParent(totalCount: 2, edges: [
+          Edge(
+              cursor: '',
+              node: Node(
+                  id: '',
+                  createdAt: 'date',
+                  bodyText: 'hi',
+                  author: Author(
+                      login: 'joe', avatarUrl: 'https://placeholder.com/250'))),
+          Edge(
+              cursor: '',
+              node: Node(
+                  id: '',
+                  createdAt: 'date',
+                  bodyText: 'hi 2',
+                  author: Author(
+                      login: 'joe', avatarUrl: 'https://placeholder.com/250')))
+        ]),
+        labels: EdgeParent(totalCount: 0, edges: []));
+    whenListen(
+      mockDetailsBloc,
+      Stream.fromIterable([testIssue]),
+      initialState: 0,
+    );
+    final pumpWidgetTest = BlocProvider<DetailsBloc>.value(
+        value: mockDetailsBloc, child: const DetailsPage(issueNumber: 2));
+    await tester.pumpWidget(pumpWidgetTest);
     await tester.pumpAndSettle();
     await tester.tap(find.byKey(issueCardFinderTest));
     await tester.pumpAndSettle();
     expect(find.byType(DetailsPage), findsOneWidget);
+    expect(find.byKey(headerKey), findsOneWidget);
+    expect(find.byKey(postBoxKey), findsOneWidget);
+    expect(find.byKey(commentBoxKey), findsNWidgets(2));
   });
 }
